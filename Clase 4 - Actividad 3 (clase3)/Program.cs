14dc714e499d542b280c8ee7f1188e9bb63b8cc2
lucas <lucas@DESKTOP-5TLC2TC>
2023-04-22 17:12:08 -0300

    Agregar archivos de proyecto.

---
+++ Clase 4 - Actividad 3 (clase3)/Program.cs
@@ -0,0 +1,31 @@
+﻿namespace actividad3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Ingrese el valor Base ");
+            int A = int.Parse(Console.ReadLine());
+
+            Console.Write("Ingrese el valor de la Potencia ");
+            int B = int.Parse(Console.ReadLine());
+
+            calcular(A, B);
+
+        }
+
+        static void calcular(int A, int B)
+        {
+            int resultado = 1;
+            int contador = 0;
+
+            while (contador < B)
+            {
+                resultado = resultado * A;
+                contador++;
+            }
+
+            Console.WriteLine("El resultado de " + A + " elevado a " + B + " es: " + resultado);
+        }
+    }
+}
